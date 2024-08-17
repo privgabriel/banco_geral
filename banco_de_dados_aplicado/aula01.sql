9968505d54d0a3851f7047ec3b5a61d50f616068
Gabriel Santos <gabriel.santos@amf.edu.br>
2024-08-17 09:44:05 -0300

    atualizações

--- banco_de_dados_aplicado/aula01.sql
+++ banco_de_dados_aplicado/aula01.sql
@@ -21,4 +21,19 @@ CREATE TABLE funcionarios (
     foreign key (dept_id) references departamentos(id),
     foreign key (cidade_id) references cidades(id)
 );
+SELECT * FROM cidades;
 
+INSERT INTO cidades (nome) VALUES ('Pelotas');
+INSERT INTO cidades (nome) VALUES ('Porto Alegre');
+INSERT INTO cidades (nome) VALUES ('São Paulo');
+INSERT INTO cidades (nome) VALUES ('Rio de Janeiro');
+
+
+INSERT INTO departamentos (descricao) VALUES ('Financeiro');
+INSERT INTO departamentos (descricao) VALUES ('Contábil');
+INSERT INTO departamentos (descricao) VALUES ('TI');
+
+INSERT INTO funcionarios (nome, dept_id, cidade_id) VALUES ('José', 1, 1);
+INSERT INTO funcionarios (nome, dept_id, cidade_id) VALUES ('Maria', 2, 3);
+INSERT INTO funcionarios (nome, dept_id, cidade_id) VALUES ('Pedro', 1, 4);
+INSERT INTO funcionarios (nome, dept_id, cidade_id) VALUES ('Ana', 2, 1);
