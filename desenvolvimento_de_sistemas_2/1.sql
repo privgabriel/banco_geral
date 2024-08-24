2609491ddded107418b70f37393856cefa9b30b7
Gabriel Santos <gabriel.santos@amf.edu.br>
2024-08-24 12:07:28 -0300

    Aula03

---
+++ desenvolvimento_de_sistemas_2/1.sql
@@ -0,0 +1,4 @@
+SELECT * FROM users;
+
+
+INSERT INTO users (id, name, email, password, birth_date, active, created_at, updated_at) VALUES (1, 'John Doe', 'exemple@gmail.com', '123456', '1990-01-01', '1', '2021-01-01 00:00:00', '2021-01-01 00:00:00');
